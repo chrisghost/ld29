097735df2cc592b90bcc75806308531c181219f6
Adrien Maillol <ama@zenexity.com>
2014-04-26 17:28:00 +0200

    Bullet/mob collision

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ function preload() {
   game.load.spritesheet('ground', 'assets/ground.png', 800, 64)
   game.load.image('player', 'assets/player.png')
   game.load.image('bkg', 'assets/bkg.png')
+  game.load.image('bullet', 'assets/bullet.png')
+  game.load.image('mob', 'assets/mob.png')
 
   game.load.script('filter', 'scripts/filters/Fire.js')
 }
@@ -11,10 +13,22 @@ function preload() {
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE)
   level.init(game)
+  mobsService.init(game)
+  mobsService.createMob(0, game)
 }
 
 function update() {
   game.physics.arcade.collide(player.instance, level.groundGrp)
+  game.physics.arcade.collide(mobsService.mobs, level.groundGrp)
+
+  game.physics.arcade.collide(player.bullets, mobsService.mobs, destroy)
   player.update()
   level.update(game, 1)
+}
+
+// Utility to destroy an object
+function destroy(a, b) {
+  console.log("destroy", a, b);
+  a.kill()
+  b.kill()
 }
--- scripts/player.js
+++ scripts/player.js
@@ -1,19 +1,34 @@
 var player = {
   instance : null
 , jumpKey : null
+, bullets : null
 , init : function(game) {
     this.jumpKey = game.input.keyboard.addKey(Phaser.Keyboard.Q)
+    this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.W)
+
+    this.bullets = game.add.group()
+    this.bullets.enableBody = true
+
     this.instance = game.add.sprite(32, game.world.height - 150, 'player')
     game.physics.arcade.enable(this.instance)
     this.instance.body.bounce.y = 0.0
     this.instance.body.gravity.y = 300
     this.instance.body.collideWorldBounds = true
-  }
 
-, update : function() {
-    if(this.jumpKey.isDown && this.instance.body.touching.down) {
+    this.jumpKey.onDown.add(this.jump, this)
+    this.fireKey.onDown.add(this.fire, this)
+  }
+, jump : function() {
+    if(this.instance.body.touching.down) {
       this.instance.body.velocity.y -= 200
     }
+}
+, update : function() {
   }
-
+, fire : function() {
+    var bullet = this.bullets.create(this.instance.x, this.instance.y, 'bullet')
+    bullet.body.velocity.x = 500
+    bullet.events.onOutOfBounds.add(function() {console.log("OUT OF BOUNDS")} , this)
+    //this.bullets.push(bullet)
+  }
 }
