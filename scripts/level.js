8383493c0a56e6ac4d06afdd48aec97819427971
Adrien Maillol <ama@zenexity.com>
2014-04-26 15:55:45 +0200

    fire and stuff

--- scripts/level.js
+++ scripts/level.js
@@ -1,7 +1,13 @@
 var level = {
   groundGrp : null
 , grounds : []
+, bkg : null
+, underWorld : false
+, underworldKey : null
+, hueRotateFilter : null
 , initWorld : function(game) {
+    this.bkg = game.add.sprite(0, 0, 'bkg')
+
     this.groundGrp = game.add.group()
     this.groundGrp.enableBody = true
     this.addGround(game, 0)
@@ -11,6 +17,7 @@ var level = {
 
     //ground.scale.setTo(800/64, 1)
     ground.body.immovable = true
+    ground.frame = this.getGroundFrame()
 
     this.grounds.push(ground)
   }
@@ -20,6 +27,10 @@ var level = {
 , init : function(game) {
     this.initWorld(game)
     this.initPlayer(game)
+
+    this.underworldKey = game.input.keyboard.addKey(Phaser.Keyboard.F)
+    this.underworldKey.onDown.add(this.toggleUnderworld, this)
+    this.fireFilter = game.add.filter('Fire', game.width, game.height)
   }
 
 , update : function(game, dx) {
@@ -30,5 +41,21 @@ var level = {
       this.grounds.shift()
     if(this.grounds[this.grounds.length-1].body.position.x < 0)
       this.addGround(game, game.width)
+
+      this.fireFilter.update()
+  }
+, toggleUnderworld : function() {
+    this.underWorld = !this.underWorld
+    if(!this.underWorld) {
+      this.bkg.filters = null
+    } else {
+      this.bkg.filters = [this.fireFilter]
+    }
+    for(i in this.grounds)
+      this.grounds[i].frame = this.getGroundFrame()
+  }
+, getGroundFrame : function() {
+    if(this.underWorld) return 1
+    return 0
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update})
 
 function preload() {
-  game.load.image('ground', 'assets/ground.png')
+  game.load.spritesheet('ground', 'assets/ground.png', 800, 64)
   game.load.image('player', 'assets/player.png')
+  game.load.image('bkg', 'assets/bkg.png')
+
+  game.load.script('filter', 'scripts/filters/Fire.js')
 }
 
 function create() {
