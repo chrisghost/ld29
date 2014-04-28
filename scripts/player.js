f7c7dffef39a0622c8996cfb074bb872a4969b1e
Adrien Maillol <ama@zenexity.com>
2014-04-28 11:00:25 +0200

    [HotFix] Typo

--- scripts/player.js
+++ scripts/player.js
@@ -78,7 +78,7 @@ var player = {
 , hit : function(p, mob) {
   player.life -= mob.hitPower
   player.weapon.nbBullets--
-  if(player.weapon.nbBullets < 1) player.weapon.fireCoolDown = 50
+  if(player.weapon.nbBullets < 1) player.weapon.nbBullets = 1
   player.weapon.fireCoolDown += 10
   if(player.weapon.fireCoolDown > 50) player.weapon.fireCoolDown = 50
   mob.kill()
